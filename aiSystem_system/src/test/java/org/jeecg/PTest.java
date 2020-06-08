6df5bf55ce1ec042bdd666533828606774e44e2d
fengjiening <271760513@qq.com>
2020-06-08 19:12:04 +0800

    测试

--- aiSystem_system/src/test/java/org/jeecg/PTest.java
+++ aiSystem_system/src/test/java/org/jeecg/PTest.java
@@ -1,5 +1,10 @@
 package org.jeecg;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
 /**
  * Created by fengjiening on 2020/5/29.
  */
@@ -9,6 +14,11 @@ public class PTest {
         Integer b =128;
 
         System.out.println(Integer.max(1,2));
+
+        Algorithm algorithm = Algorithm.HMAC256("1234561");
+        JWTVerifier verifier = JWT.require(algorithm).withClaim("username", "admin").build();
+        // 效验TOKEN
+        DecodedJWT jwt = verifier.verify("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1OTE2MDU4MjAsInVzZXJuYW1lIjoiYWRtaW4ifQ.XXaU_C0TcXFEKVhTYjvVNzI9EkYsfhNgkm7mNoPFEa4");
 
     }
 }
