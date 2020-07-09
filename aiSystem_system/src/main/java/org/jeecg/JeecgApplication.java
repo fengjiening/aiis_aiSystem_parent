70f6f976f3b8c9eaa40c1586096aa468593690d0
fengjiening <271760513@qq.com>
2020-07-09 15:21:10 +0800

    测试2

--- aiSystem_system/src/main/java/org/jeecg/JeecgApplication.java
+++ aiSystem_system/src/main/java/org/jeecg/JeecgApplication.java
@@ -3,14 +3,22 @@ package org.jeecg;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.Context;
 import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.jeecg.modules.message.websocket.WebSocket;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.util.WebAppRootListener;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -21,8 +29,16 @@ public class JeecgApplication {
 
   public static void main(String[] args) throws UnknownHostException {
 
-    ConfigurableApplicationContext application = SpringApplication.run(JeecgApplication.class, args);
-    Environment env = application.getEnvironment();
+   // ConfigurableApplicationContext application = SpringApplication.run(JeecgApplication.class, args);
+
+      SpringApplication springApplication = new SpringApplication(JeecgApplication.class); //这里填你的Class名字
+      ConfigurableApplicationContext run = springApplication.run(args);
+      WebSocket.setApplicationContext(run);
+
+
+
+
+    Environment env = WebSocket.applicationContext.getEnvironment();
     String ip = InetAddress.getLocalHost().getHostAddress();
     String port = env.getProperty("server.port");
     String path = env.getProperty("server.servlet.context-path");
@@ -47,4 +63,17 @@ public class JeecgApplication {
     }
    };
   }
+
+    @Configuration
+    @ComponentScan
+    @EnableAutoConfiguration
+    public class WebAppRootContext implements ServletContextInitializer {
+        @Override
+        public void onStartup(ServletContext servletContext) throws ServletException {
+            System.out.println("org.apache.tomcat.websocket.textBufferSize");
+            servletContext.addListener(WebAppRootListener.class);
+            servletContext.setInitParameter("org.apache.tomcat.websocket.textBufferSize","1024000");
+        }
+  }
+
 }
